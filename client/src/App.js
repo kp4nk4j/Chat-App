56e811c2ab4433d90d62a9edfd709fb6c5defe13
kp4nk4j <pankajkvs2001@gmail.com>
2022-09-13 13:09:48 +0530

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import SetAvatar from "./components/SetAvatar";
+import Chat from "./pages/Chat";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/register" element={<Register />} />
+        <Route exact path="/login" element={<Login />} />
+        <Route exact path="/setAvatar" element={<SetAvatar />} />
+        <Route exact path="/" element={<Chat />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
